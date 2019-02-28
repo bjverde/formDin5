9af09ac4fa2995704e750d690ce13e2d0d8e6747
Bjverde <bjverde@yahoo.com.br>
2019-02-28 01:38:10 -0300

    :hammer: BASE #6 melhoria na geração do Menu

--- base/core/TMenuBootStrap.class.php
+++ base/core/TMenuBootStrap.class.php
@@ -99,20 +99,6 @@ class TMenuBootStrap {
         return $button;
     }
 
-    public function getNavUl()
-	{
-        $navUl = new TElement('ul');
-        $navUl->setClass('navbar-nav mr-auto');
-        return $navUl;
-    }    
-
-    public function getNavCollapse()
-	{
-        $navColl = new TDiv('navbarsExampleDefault');
-        $navColl->setClass('collapse navbar-collapse');
-        return $navColl;
-    }
-
     public function getNav2()
 	{
         $nav = new TElement('nav');
@@ -122,95 +108,195 @@ class TMenuBootStrap {
         return $nav;
     }
 
-   public function getObjXmlMenu($menuFile)
-   {
-     if ( !file_exists($menuFile) ) {
-         throw new InvalidArgumentException (TMesage::MENU_FILE_FAIL);
-     }
-     ob_start();
-     include $menuFile;
-     $result = ob_get_clean();
-     ob_end_clean();
-     $xmlMenu = simplexml_load_string($result);
-     return $xmlMenu;
-   }
-
-   public function getArrayMenu($menuFile)
-   {
-    $xmlMenu = $this->getObjXmlMenu($menuFile);
-    $jsonMenu = json_encode($xmlMenu);
-    $arrayMenu = json_decode($jsonMenu,TRUE);
-    return $arrayMenu;
-   }
-
-   public function buildItem($item)
-   {
-       //var_dump($item);
-        $text = $item['@attributes']['text'];
+    
+    public function buildItem($item)
+    {
+        //var_dump($item);
+        //$text = $item['@attributes']['text'];
         $item = new TElement('a');
         $item->setClass('dropdown-menu');
         $item->setAttribute('href','#');
         $item->add($text);
         return $item;
-   }
-
-   public function buildNavLink($item)
-   {
-       //var_dump($item['@attributes']['img']);
-       
-       $img = null;
-       if(ArrayHelper::has('img',$item['@attributes'])){
-        $imgCaminho = $this->getMenuIconsPath();   
-        $imgCaminho = $imgCaminho.$item['@attributes']['img'];
+    }
+    
+    
+    /***
+     * Return HTML5 Element img or Font
+     * @param string $imgString
+     * @return TElement
+     */
+    public function getMenuIcon($imgString){
+        $imgCaminho = $this->getMenuIconsPath();
+        $imgCaminho = $imgCaminho.$imgString;
         $img = new TElement('img');
         $img->setClass('menuIcon');
         $img->setAttribute('src',$imgCaminho);
-       }       
-
+        return $img;
+    }
+    
+    
+    /**
+     * Return HTML5 Element LU
+     * @param string $id
+     * @param array $subItens
+     * @return TElement
+     */
+    public function getUlDropMenu($id,$subItens){
+        $dropMenu = new TElement('ul');
+        $dropMenu->setClass('dropdown');
+        $dropMenu->setAttribute('aria-labelledby',$id);
+        return $dropMenu;
+    }
+    
+    public function buildNavLink($item)
+    {
+        //ini_set('xdebug.var_display_max_depth', '10');
+        //ini_set('xdebug.var_display_max_children', '256');
+        //ini_set('xdebug.var_display_max_data', '-1');
+        //var_dump($item);exit();
+        //var_dump($item['@attributes']);
+        $id = $item['@attributes']['id'];
+        
+        $img = null;
+        if(ArrayHelper::has('img',$item['@attributes'])){
+            $img = $this->getMenuIcon($item['@attributes']['img']);
+        }
+        
+        $subItens = null;
+        if(ArrayHelper::has('item',$item)){
+            $subItens = ArrayHelper::getArray($item, 'item');
+        }
+        
         $text = $item['@attributes']['text'];
         $item = new TElement('a');
-        $item->setClass('nav-link');
+        If( CountHelper::count($subItens) > 0 ){
+            $item->setAttribute('id',$id);
+            $item->setClass('nav-link dropdown-toggle');
+            $item->setAttribute('data-toggle','dropdown');
+            $item->setAttribute('aria-haspopup','true');
+            $item->setAttribute('aria-expanded','false');
+        }else{
+            $item->setClass('nav-link');
+        }
         $item->setAttribute('href','#');
         if(!empty($img)){
             $item->add($img);
         }
         $item->add($text);
         return $item;
-   }   
-
-   public function buildNavItem($item)
-   {
+    }
+    
+    /***
+     * Return HTML5 Element LI
+     * @param array $item
+     * @return TElement
+     */
+    public function buildNavItem($item)
+    {
+        $subItens = null;
+        $subMenu = null;
+        if(ArrayHelper::has('item',$item)){
+            $id = $item['@attributes']['id'];
+            $subItens = ArrayHelper::getArray($item, 'item');
+            $subMenu = $this->getUlDropMenu($id, $subItens);
+        }
+        
         $navLinks = $this->buildNavLink($item);
-
+        
         $item = new TElement('li');
         $item->setClass('nav-item');
         $item->add($navLinks);
+        If( CountHelper::count($subItens) > 0 ){
+            $item->add($subMenu);
+        }
         return $item;
-   }   
+    }
+    
+    /***
+     * Return HTML5 Element UL 
+     * @return TElement
+     */
+    public function getNavUl()
+    {
+        $navUl = new TElement('ul');
+        $navUl->setClass('navbar-nav mr-auto');
+        return $navUl;
+    } 
+    
+    /***
+     * Return HTML5 Element Div with
+     * Nav Bar Collapse
+     * @return TDiv
+     */
+    public function getNavCollapse()
+    {   
+        $navColl = new TDiv('navbarCollapse');
+        $navColl->setClass('collapse navbar-collapse');
+        return $navColl;
+    }
+    
+    /***
+     * Return Objetc SimpleXMLElement with Menu
+     * @param string $menuFile - path file menu
+     * @throws InvalidArgumentException
+     * @return SimpleXMLElement
+     */
+    public function getObjXmlMenu($menuFile)
+    {
+        if ( !file_exists($menuFile) ) {
+            throw new InvalidArgumentException (TMesage::MENU_FILE_FAIL);
+        }
+        
+        ob_start();
+        include $menuFile;
+        $result = ob_get_clean();
+        ob_end_clean();
+        
+        $xmlMenu = simplexml_load_string($result);
+        return $xmlMenu;
+    }
 
-	public function getMenuBootStrap($menuFile, $print=true)
-	{
+    /**
+     * Return Array PHP with Menu
+     * @param string $menuFile - path file menu
+     * @return mixed
+     */
+    public function getArrayMenu($menuFile)
+    {
+        $xmlMenu = $this->getObjXmlMenu($menuFile);
+        $jsonMenu = json_encode($xmlMenu);
+        $arrayMenu = json_decode($jsonMenu,TRUE);
+        return $arrayMenu;
+    }
 
-    ini_set('xdebug.var_display_max_depth', '10');
-    ini_set('xdebug.var_display_max_children', '256');
-    ini_set('xdebug.var_display_max_data', '-1');
-    //var_dump($xmlMenu);        
-
-     $arrayMenu = $this->getArrayMenu($menuFile);
-
-     $navUl = $this->getNavUl();
-      foreach($arrayMenu['item'] as $key => $item) {
-         //echo $key;
-         //var_dump($item);
-         //print_r($item);
-         $ItemMenu = $this->buildNavItem($item);
-         $navUl->add($ItemMenu);
-      }
-      $navColl = $this->getNavCollapse();
-      $navColl->add($navUl);
-      $nav = $this->getNav2();
-      $nav->add($navColl);
-      return $nav;
-	}
+    /***
+     * Create Element HTML 5 for use BootStrap CSS Menu
+     * @param string $menuFile - path file menu
+     * @param boolean $print
+     * @return TElement
+     */
+    public function getMenuBootStrap($menuFile, $print=true)
+    {    
+        //ini_set('xdebug.var_display_max_depth', '10');
+        //ini_set('xdebug.var_display_max_children', '256');
+        //ini_set('xdebug.var_display_max_data', '-1');
+        //var_dump($xmlMenu);
+        
+        $arrayMenu = $this->getArrayMenu($menuFile);
+        $navUl = $this->getNavUl();
+        foreach($arrayMenu['item'] as $key => $item) {
+            //echo $key;
+            //var_dump($item);
+            //print_r($item);
+            $ItemMenu = $this->buildNavItem($item);
+            $navUl->add($ItemMenu);
+        }
+        $navColl = $this->getNavCollapse();
+        $navColl->add($navUl);
+        $nav = $this->getNav2();
+        $nav->add($navColl);
+        return $nav;
+    }
 }
 ?>
