dada1a201d5dc0d4f2f45efbd3457f1dff77df80
Bjverde <bjverde@yahoo.com.br>
2019-05-05 15:29:09 -0300

    :hammer: BASE FIXME #17

--- base/test/phpunit_coverage.bat
+++ base/test/phpunit_coverage.bat
@@ -1,13 +1,12 @@
 REM 6.4.3 Final
 REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\phpunit\phpunit-6.4.3.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\ph
 
-REM 7.2.4 Final
+ECHO 7.2.4 Coverage
 REN D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --whitelist D:\wamp\www\formDin5\base --coverage-html D:\wamp\www\formDin5\phpunit-code-coverage D:\wamp\www\formDin5\base\test\
 
-REM 8.1.3 Final
-REN D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-8.1.3.phar --whitelist D:\wamp\www\formDin\base\ --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
+ECHO 7.2.4 Config XML and Coverage
+REN D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin5\base\test\phpunit-conf.xml --coverage-html D:\wamp\www\formDin5\phpunit-code-coverage D:\wamp\www\formDin5\base\test\
 
-REM 7.2.4 Config file
-REN D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin5\base\test\phpunit-conf.xml D:\wamp\www\formDin5\base\test\
+ECHO 7.2.4 Config XML file
+D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin5\base\test\phpunit-conf.xml D:\wamp\www\formDin5\base\test\
 
-D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin5\base\test\phpunit-conf.xml --coverage-html D:\wamp\www\formDin5\phpunit-code-coverage D:\wamp\www\formDin5\base\test\
