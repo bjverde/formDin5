b4ce7a4ad37790ac1bbe843aaffc2125a4bfadb5
Bjverde <bjverde@yahoo.com.br>
2019-11-09 18:06:13 -0300

    :memo: EX #53

---
+++ ref_bootstrap/ex_TControl.php
@@ -0,0 +1,51 @@
+<?php 
+
+if(!defined('ENCODINGS') ) { define('ENCODINGS', 'UTF-8'); 
+}
+
+require_once '../base/core/webform/TElement.class.php';
+require_once '../base/core/webform/TControl.class.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../aa_libs/bootstrap/favicon.ico">
+    <title>Exemplo Menu Side</title>
+
+
+    <link rel="stylesheet" type="text/css" href="../base/lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../base/lib/font-awesome/css/fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="../base/lib/font-awesome/css/fontawesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../base/lib/font-awesome/css/fa-brands.min.css">
+    <link rel="stylesheet" type="text/css" href="../base/lib/font-awesome/css/brands.min.css">
+    <link rel="stylesheet" type="text/css" href="../base/lib/font-awesome/css/fa-regular.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="formCustom.css">    
+
+
+</head>
+<body>
+
+<?php
+echo 'xxx';
+echo '<br>';
+$obj = new TControl();
+
+
+?>
+	<!-- ================================================== -->
+	<!-- Bootstrap core JavaScript -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../base/lib/jquery/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../base/lib/jquery/jquery.min.js"><\/script>')</script>
+    <script src="../base/lib/bootstrap/assets/js/vendor/popper.min.js"></script>
+    <script src="../base/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../base/lib/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
