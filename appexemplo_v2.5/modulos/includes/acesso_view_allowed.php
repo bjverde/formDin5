4195553f4a9355d696731bdc7d6058267b31f289
Bjverde <bjverde@yahoo.com.br>
2020-04-11 20:22:30 -0300

    :fire: removendo arquivos

--- appexemplo_v2.5/modulos/includes/acesso_view_allowed.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Controla se a pessoa pode acessar o modulo ou não
- */
-if ( Acesso::viewAccessNotAllowed($_REQUEST['modulo']) ){
-    die();
-}
--- ref_bootstrap/test_poo/teste2.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class teste2 extends teste
-{
-    public function testando()
-    {
-        parent::testando();
-        echo 3;
-    }
-}
