be48817fb618016f22e9169756a5f65f078d9347
Bjverde <bjverde@yahoo.com.br>
2020-04-12 19:45:20 -0300

    :memo: BASE #60 TFormDin melhorando documentação

--- appexemplo_v1.0/app/control/formdin5/webform/TFormDin.class.php
+++ appexemplo_v1.0/app/control/formdin5/webform/TFormDin.class.php
@@ -1,10 +1,57 @@
 <?php
+/*
+ * ----------------------------------------------------------------------------
+ * Formdin 5 Framework
+ * SourceCode https://github.com/bjverde/formDin5
+ * @author Reinaldo A. Barrêto Junior
+ * 
+ * É uma reconstrução do FormDin 4 Sobre o Adianti 7.1
+ * ----------------------------------------------------------------------------
+ * This file is part of Formdin Framework.
+ *
+ * Formdin Framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not,  see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * Este arquivo é parte do Framework Formdin.
+ *
+ * O Framework Formdin é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * do Software Livre (FSF).
+ *
+ * Este programa é distribuí1do na esperança que possa ser útil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO EM PARTICULAR. Veja a Licen?a Pública Geral GNU/LGPL em portugu?s
+ * para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
 
 /**
  * Classe para criação de formulários web para entrada de dados
+ * ------------------------------------------------------------------------
+ * Esse é o FormDin 5, que é uma reconstrução do FormDin 4 Sobre o Adianti 7.1
  * 
- * Esse é o FormDin 5, que é um reconstrução do 
- * FormDin 4.8 sobre o Adianti 7.1
+ * FormDin 5 - Alguns parametros estão marcados como DEPRECATED por não 
+ * funcionar no Adianti foram mantidos para diminuir o impacto sobre
+ * as migrações.
+ *
+ * FORMDIN5 = Parametro novo disponivel apenas na nova versão
+ * NOT_IMPLEMENTED = parametros que serão implementados em versões futuras
+ * ------------------------------------------------------------------------
  * 
  * @author Reinaldo A. Barrêto Junior
  */
@@ -15,7 +62,7 @@ class TFormDin
     /**
      * Método construtor da classe do Formulario Padronizado em BoorStrap
      * ------------------------------------------------------------------------
-     * Reconstruido FormDin 4 Sobre o Adianti 7.1
+     * Esse é o FormDin 5, que é uma reconstrução do FormDin 4 Sobre o Adianti 7.1
      * 
      * FormDin 5 - Alguns parametros estão marcados como DEPRECATED por não 
      * funcionar no Adianti foram mantidos para diminuir o impacto sobre
