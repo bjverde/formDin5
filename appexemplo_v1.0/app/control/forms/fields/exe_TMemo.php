8cb09554203543cf8c8e4b009126f9b965f8352b
bjverde <bjverde@yahoo.com.br>
2020-04-30 02:38:49 -0300

    :hammer: APP #71 teste contador

--- appexemplo_v1.0/app/control/forms/fields/exe_TMemo.php
+++ appexemplo_v1.0/app/control/forms/fields/exe_TMemo.php
@@ -24,6 +24,39 @@ class exe_TMemo extends TPage
         
         $this->form = $frm->show();
 
+        $detalhesolicitacao = new TText('detalhesolicitacao');
+        $detalhesolicitacao->maxlength = '2000';
+        $detalhesolicitacao->setProperty('onkeyup', 'fwCheckNumChar(this,1000);');
+        $chars                 = new TElement('span');
+        $chars->id             = 'chars';
+        $script             = new TElement('script');
+        $script->type = 'text/javascript';
+        $script->add("var maxLength = 2000;
+                        $('textarea').keyup(function() {
+                          var length = $(this).val().length;
+                          var length = maxLength-length;
+                          $('#chars').text(length+' Caracteres Restantes');
+                        });");
+        $container = new TVBox;
+        $container->add($detalhesolicitacao);
+        $container->add($chars);
+        $container->add($script);
+
+        $this->form->addFields([new TLabel('xxxx')], [$container]);
+
+        $det02    = new TText('det02');
+        $det02->setProperty('onkeyup', 'fwCheckNumChar(this,1000);');
+        $chars02  = new TElement('span');
+        $script02 = new TElement('script');
+        $script02->setProperty('src', 'app/lib/include/FormDin5.js');
+        $div02    = new TElement('div');
+        $div02->add($det02);
+        $div02->add($chars02);
+        $div02->add($script02);
+
+        $this->form->addFields([new TLabel('xxxx22')], [$div02]);
+
+
         $this->form->setData( TSession::getValue(__CLASS__.'_filter_data'));
 
         // add form actions
