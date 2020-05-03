fb0eccde321242e200e16fd00c6e9b0e913f388f
bjverde <bjverde@yahoo.com.br>
2020-05-03 02:06:39 -0300

    :hammer: APP #60 novas telas

---
+++ appexemplo_v1.0/app/control/forms/fields/exe_DateField.php
@@ -0,0 +1,64 @@
+<?php
+
+use Adianti\Registry\TSession;
+
+class exe_DateField extends TPage
+{
+    protected $form; // registration form
+    protected $datagrid; // listing
+    protected $pageNavigation;
+    
+    // trait com onReload, onSearch, onDelete...
+    use Adianti\Base\AdiantiStandardListTrait;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $frm = new TFormDin('Exemplo de Entrada de Dados com Máscara');
+
+        $frm->addMaskField('c1', 'Código:', false, '99.99.99', null, null, null, null, '99.99.99');
+        $frm->addMaskField('c2', 'Placa do Carro:', false, 'aaa-9999')->setExampleText('aaa-9999');
+        $frm->addMaskField('c3', 'Código de Barras:', false, '9 999999 999999')->setExampleText('9 999999 999999');
+
+        $this->form = $frm->show();
+
+        $this->form->setData( TSession::getValue(__CLASS__.'_filter_data'));
+
+        // add form actions
+        // O Adianti permite a Internacionalização - A função _t('string') serve
+        //para traduzir termos no sistema. Veja ApplicationTranslator escrevendo
+        //primeiro em ingles e depois traduzindo
+        $this->form->addAction(_t('Save'), new TAction(array($this, 'onSave')), 'far:check-circle green');
+        $this->form->addActionLink(_t('Clear'),  new TAction([$this, 'clear']), 'fa:eraser red');
+
+        // creates the page structure using a table
+        $formDinBreadCrumb = new TFormDinBreadCrumb(__CLASS__);
+        $vbox = $formDinBreadCrumb->getAdiantiObj();
+        $vbox->add($this->form);
+        
+        // add the table inside the page
+        parent::add($vbox);
+    }
+
+    /**
+     * Clear filters
+     */
+    public function clear()
+    {
+        $this->clearFilters();
+        $this->onReload();
+    }
+
+    public function onSave($param)
+    {
+        $data = $this->form->getData();
+        $this->form->setData($data);
+
+        //Função do FormDin para Debug
+        FormDinHelper::d($param,'$param');
+        FormDinHelper::debug($data,'$data');
+        FormDinHelper::debug($_REQUEST,'$_REQUEST');
+    }
+
+}
