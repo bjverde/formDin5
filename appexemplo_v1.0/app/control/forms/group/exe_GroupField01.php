160f27d01aad16b155f429034b9777d267c39689
bjverde <bjverde@yahoo.com.br>
2020-06-12 02:09:16 -0300

    :memo: APP #75 exemplo

--- appexemplo_v1.0/app/control/forms/group/exe_GroupField01.php
+++ appexemplo_v1.0/app/control/forms/group/exe_GroupField01.php
@@ -16,12 +16,13 @@ class exe_GroupField01 extends TPage
         parent::__construct();
 
         $frm = new TFormDin('Exemplo de Grupos');
+        $frm->addGroupField('gpx1', 'Teste Novo Grupo 1');
         //$frm->addGroupField('gpx1', 'Teste Novo Grupo 1')->setLabelsAlign('center');
-        $frm->addTextField('nome1', 'Nome 1:', 40, true);
-        $frm->addTextField('nome2', 'Nome 2:', 40, true);
-        $frm->addTextField('nome3', 'Nome 3:', 40, true);
-        //$frm->addButton('Gravar', null, 'btnGravar', 'alert(0)', null, true, false)->setAttribute('align', 'left');
-        //$frm->closeGroup();
+            $frm->addTextField('nome1', 'Nome 1:', 40, true);
+            $frm->addTextField('nome2', 'Nome 2:', 40, true);
+            $frm->addTextField('nome3', 'Nome 3:', 40, true);
+            //$frm->addButton('Gravar', null, 'btnGravar', 'alert(0)', null, true, false)->setAttribute('align', 'left');
+        $frm->closeGroup();
                 
 
         // O Adianti permite a Internacionalização - A função _t('string') serve
@@ -30,28 +31,6 @@ class exe_GroupField01 extends TPage
         $frm->setAction(_t('Save'),'onSave',$this,null,'far:check-circle','green');
         $frm->setActionLink(_t('Clear'),'clear',$this,null,'fa:eraser','red');
         $this->form = $frm->show();
-
-
-        $subform = new BootstrapFormBuilder;
-
-        // creates a frame
-        $frame = new TFrame;
-        $frame->oid = 'frame-measures';
-        $frame->setLegend('Measures');
-        $frame->add( $subform );
-
-        $subform->addFields([new TLabel('Net Weight:')], [new TEntry('field1')]);
-
-
-        // wrap the page content using vertical box
-        //$vboxFrame = new TVBox;
-        //$vboxFrame->style = 'width: 100%';
-        //$vboxFrame->add($frame);
-        //$vboxFrame->add( [new TLabel('Net Weight:')], [new TEntry('field1')]);
-
-        $this->form->addContent([$frame]);
-
-
         
 
         $this->form->setData( TSession::getValue(__CLASS__.'_filter_data'));
