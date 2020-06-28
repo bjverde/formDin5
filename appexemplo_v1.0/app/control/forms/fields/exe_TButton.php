286c3d5fa90cbbb8ab088e67788782cb04968b45
bjverde <bjverde@yahoo.com.br>
2020-06-28 20:16:33 -0300

    :memo: APP #86 exemplo

--- appexemplo_v1.0/app/control/forms/fields/exe_TButton.php
+++ appexemplo_v1.0/app/control/forms/fields/exe_TButton.php
@@ -16,36 +16,38 @@ class exe_TButton extends TPage
         parent::__construct();
 
         $frm = new TFormDin('Exemplo Mensagem apenas PHP');
-        $frm->setAction('Nome Botão','onSave',$this);
+        //$frm->setAction('Nome Botão','onSave',$this);
+        $frm->setAction(_t('Save'), 'onSave',$this, false,'far:check-circle green');
+        $frm->setAction(_t('Clear'), 'clear',$this, false,'fa:eraser red');
         $frm->setAction('YYYY','onSave',$this,true,null,'green');
         
         
-        $frm->addTextField('entry','TEntry', 10);
-        $frm->addButton($this,'Success',null,'onSave',null,null,false,false);
+        $frm->addTextField('entry2','TEntry', 10);
+        $frm->addButton($this,'Success1',null,'onSave3',null,null,false,false);
 
-        $frm->addButton($this,'Limpar', null, 'Limpar', null, null, false, false);
-        $frm->addButton($this,'Exemplo 09 - img', null, 'act09', null, null, true, false,'joia.gif');
-        $frm->addButton($this,'Exemplo 09 - img desabilitado', null, 'act09d', null, null, true, false,'joia.gif','joia_desabilitado.gif');
-        $frm->addButton($this,'Exemplo 10 - img', null, 'act10', null, null, true, false,'favicon-32x32.png');
-        $frm->addButton($this,'Exemplo 11 - hint', null, 'act11', null, null, true, false,null,null,'text hint');
-        $frm->addButton($this,'Exemplo 12', null, 'act12', null, null, true, false,null,null,null,null,true,'xxx','center');
-        $frm->addButton($this,'Exemplo 13', null, 'act13', null, null, true, false,null,null,null,null,true,'xxx','left');
-        $frm->addButton($this,'Exemplo 14', null, 'act14', null, null, true, false,null,null,null,null,null,null,'left');
+        $frm->addTextField('txt','Text1', 10);
+        $frm->addButton($this,'Nova linha',null,'onSave1',null,null,true,false,'far:check-circle green');
+        $frm->addButton($this,'mesma linha',null,'onSave2',null,null,false,false);
+
+
+        //$frm->addButton($this,'Limpar', null, 'Limpar', null, null, false, false);
+        //$frm->addButton($this,'Exemplo 09 - img', null, 'act09', null, null, true, false,'joia.gif');
+        //$frm->addButton($this,'Exemplo 09 - img desabilitado', null, 'act09d', null, null, true, false,'joia.gif','joia_desabilitado.gif');
+        //$frm->addButton($this,'Exemplo 10 - img', null, 'act10', null, null, true, false,'favicon-32x32.png');
+        //$frm->addButton($this,'Exemplo 11 - hint', null, 'act11', null, null, true, false,null,null,'text hint');
+        //$frm->addButton($this,'Exemplo 12', null, 'act12', null, null, true, false,null,null,null,null,true,'xxx','center');
+        //$frm->addButton($this,'Exemplo 13', null, 'act13', null, null, true, false,null,null,null,null,true,'xxx','left');
+        //$frm->addButton($this,'Exemplo 14', null, 'act14', null, null, true, false,null,null,null,null,null,null,'left');
         $frm->addTextField('xxx','xxx',30);
-
-
-        //$frm->addButton($this,'Success',null,'onSave');
-
         $this->form = $frm->show();
-        
-        //$hbox2 = new THBox;
-        //$hbox2->addRowSet( $label, $entry, $bt3c );
-        //$frame1 = new TFrame;
-        //$frame1->setLegend('Font awesome icons');
-        //$frame1->add($hbox2);
 
-        //$this->form->addFields( [ new TLabel('TEntry') ],   [ $entry ],[$bt3c]);
-        //$this->form->addFields( [ $hbox2]);
+
+        $label = new TLabel('Text Exemplo');
+        $entry = new TEntry('entry');
+        $action = new TAction(array($this, 'onSave'));
+        $bt3c = new TButton('bt3c');
+        $bt3c->setAction($action,'Btn Exemplo');
+        $this->form->addFields( [$label], [$entry], [$bt3c]);
        
 
         $this->form->setData( TSession::getValue(__CLASS__.'_filter_data') );
@@ -78,5 +80,38 @@ class exe_TButton extends TPage
         FormDinHelper::debug($data,'$data');
         FormDinHelper::debug($_REQUEST,'$_REQUEST');
     }
+
+    public function onSave1($param)
+    {
+        $data = $this->form->getData();
+        $this->form->setData($data);
+
+        //Função do FormDin para Debug
+        FormDinHelper::d($param,'$param');
+        FormDinHelper::debug($data,'$data');
+        FormDinHelper::debug($_REQUEST,'$_REQUEST');
+    }
+
+    public function onSave2($param)
+    {
+        $data = $this->form->getData();
+        $this->form->setData($data);
+
+        //Função do FormDin para Debug
+        FormDinHelper::d($param,'$param');
+        FormDinHelper::debug($data,'$data');
+        FormDinHelper::debug($_REQUEST,'$_REQUEST');
+    }
+
+    public function onSave3($param)
+    {
+        $data = $this->form->getData();
+        $this->form->setData($data);
+
+        //Função do FormDin para Debug
+        FormDinHelper::d($param,'$param');
+        FormDinHelper::debug($data,'$data');
+        FormDinHelper::debug($_REQUEST,'$_REQUEST');
+    }    
 
 }
