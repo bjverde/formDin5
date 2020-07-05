3c15737d255f5adbccca87fe4516b452d25beee2
bjverde <bjverde@yahoo.com.br>
2020-07-05 01:53:36 -0300

    :memo:  APP #102

--- appexemplo_v1.0/app/control/forms/dialogue/exe_mensagemv2.php
+++ appexemplo_v1.0/app/control/forms/dialogue/exe_mensagemv2.php
@@ -16,9 +16,16 @@ class exe_mensagemv2 extends TPage
         parent::__construct();
 
         $frm = new TFormDin('Exemplo Mensagem apenas PHP');
-        $text[] = 'linha1';
-        $text[] = "linha2 <b>'xxx'</b> linha2";
-        $frm->setMessage($text,TFormDinMessage::TYPE_WARING);
+        //$text[] = 'linha1';
+        //$text[] = "linha2 <b>'xxx'</b> linha2";
+        //$frm->setMessage($text,TFormDinMessage::TYPE_WARING);
+
+        $frm->addButton($this,'Msg Alert', null, 'msgAlert',  null, null, true, false);
+        $frm->addButton($this,'Msg Info', null, 'msgInfo',  null, null, true, false);
+        $frm->addButton($this,'Msg Error', null, 'msgError',  null, null, true, false);
+        //$frm->addButton($this,'Msg POP Sucesso', null, 'msgpopsu',  null, null, false, false);
+        //$frm->addButton($this,'Msg POP Error', null, 'msgpoperror', null, null, false, false);
+        //$frm->addButton($this,'Msg POP Attention', null, 'msgpopattention', null, null, false, false);
         
         $this->form = $frm->show();
 
@@ -28,7 +35,7 @@ class exe_mensagemv2 extends TPage
         // O Adianti permite a Internacionalização - A função _t('string') serve
         //para traduzir termos no sistema. Veja ApplicationTranslator escrevendo
         //primeiro em ingles e depois traduzindo
-        $this->form->addAction(_t('Save'), new TAction(array($this, 'onSave')), 'far:check-circle green');
+        //$this->form->addAction(_t('Save'), new TAction(array($this, 'onSave')), 'far:check-circle green');
         $this->form->addActionLink(_t('Clear'),  new TAction([$this, 'clear']), 'fa:eraser red');
 
         // creates the page structure using a table
@@ -49,15 +56,29 @@ class exe_mensagemv2 extends TPage
         $this->onReload();
     }
 
-    public function onSave($param)
+    public function msgAlert()
     {
-        $data = $this->form->getData();
-        $this->form->setData($data);
+        $text[] = 'linha1 de mensagem em HTML';
+        $text[] = "linha2 <b>'xxx'</b> linha2";
+        $text = TFormDinMessage::messageTransform($text);
+        new TMessage(TFormDinMessage::TYPE_WARING, $text);
+        //new TMessage(TFormDinMessage::TYPE_WARING, AdiantiCoreTranslator::translate('Record saved'));
+    }
 
-        //Função do FormDin para Debug
-        FormDinHelper::d($param,'$param');
-        FormDinHelper::debug($data,'$data');
-        FormDinHelper::debug($_REQUEST,'$_REQUEST');
+    public function msgInfo()
+    {
+        $text[] = 'linha1 de mensagem em HTML';
+        $text[] = "linha2 <b>'xxx'</b> linha2";
+        $text = TFormDinMessage::messageTransform($text);
+        new TMessage(TFormDinMessage::TYPE_INFO, $text);
+    }
+
+    public function msgError()
+    {
+        $text[] = 'linha1 de mensagem em HTML <span style="color:red">falando que deu MMM</span>';
+        $text[] = "linha2 <b>'xxx'</b> linha2";
+        $text = TFormDinMessage::messageTransform($text);
+        new TMessage(TFormDinMessage::TYPE_ERROR, $text);
     }
 
 }
