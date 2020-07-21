ec27452ef1e776795a308e10758529a702279fb5
bjverde <bjverde@yahoo.com.br>
2020-07-21 03:14:26 -0300

    :bug: PHPUnit comand line win

--- appexemplo_v1.0/app/tests/phpunit_coverage.bat
+++ appexemplo_v1.0/app/tests/phpunit_coverage.bat
@@ -28,7 +28,7 @@ REM D:\wamp\bin\php\php7.3.5\php.exe D:\wamp\bin\phpunit\phpunit-8.1.3.phar --co
 REM ---------------- 9.1.4 -------------------------
 
 ECHO PHP 7.3.5 and PHPUnit 9.1.4 Simples
-D:\wamp\bin\php\php7.3.5\php.exe D:\wamp\bin\phpunit\phpunit-9.1.4.phar --colors=auto --bootstrap D:\wamp\www\formDin5\appexemplo_v1.0\init.php D:\wamp\www\formDin5\appexemplo_v1.0\app\tests\
+D:\wamp\bin\php\php7.3.5\php.exe D:\wamp\bin\phpunit\phpunit-9.1.4.phar --colors=auto --bootstrap D:\wamp\www\adianti\formDin5\appexemplo_v1.0\init.php D:\wamp\www\adianti\formDin5\appexemplo_v1.0\app\tests\
 
 REM ECHO PHP 7.3.5 and PHPUnit 9.1.4 Simples with Coverage
 REM D:\wamp\bin\php\php7.3.5\php.exe D:\wamp\bin\phpunit\phpunit-9.1.4.phar --colors=auto --bootstrap D:\wamp\www\formDin5\appexemplo_v1.0\init.php --whitelist D:\wamp\www\formDin5\appexemplo_v1.0\app\lib\widget\FormDin5 --coverage-html D:\wamp\www\formDin5\phpunit-code-coverage D:\wamp\www\formDin5\appexemplo_v1.0\app\tests\
