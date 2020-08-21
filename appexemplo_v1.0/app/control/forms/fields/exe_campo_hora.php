ddc8d5a8e4fc4d821d095f2d347f575cac73012e
bjverde <bjverde@yahoo.com.br>
2020-08-21 01:58:31 -0300

    :hammer: #144 include field

--- appexemplo_v1.0/app/control/forms/fields/exe_campo_hora.php
+++ appexemplo_v1.0/app/control/forms/fields/exe_campo_hora.php
@@ -15,22 +15,27 @@ class exe_campo_hora extends TPage
     {
         parent::__construct();
 
-        $frm = new TFormDin($this,'Exemplo de Entrada de Dados com Máscara');
+        $frm = new TFormDin($this,'Exemplo Campo Hora ');
 
-        $frm->addMaskField('c1', 'Código:', false, '99.99.99', null, null, null, null, '99.99.99');
-        $frm->addMaskField('c2', 'Placa do Carro:', false, 'aaa-9999')->setExampleText('aaa-9999');
-        $frm->addMaskField('c3', 'Código de Barras:', false, '9 999999 999999')->setExampleText('9 999999 999999');
+        $html = '<br><br><b>Regra de Negocio</b>'
+               .'<br>Passe o mouse no balões de ajuda e descubra a regra';
 
-        $this->form = $frm->show();
+        //$frm->addHtmlField('texto', $html)->setCss('border', '1px solid red');
+        $frm->addHtmlField('texto', $html);
+        //$frm->addTextField('nom_pessoa', 'Nome:', 50, false)->setEnabled(false);
+        $frm->addTextField('nom_pessoa', 'Nome:', 50, false);
+        $fld = $frm->addTimeField('hor_entrada', 'Hora Entrada:', true, '05:00', '23:00', '99:99');
+        $fld->setToolTip('Hora no formato HH:MM entre 05:00 e 23:00');
+        $frm->addTimeField('hor_inicio2', 'Hora:', false, '12:00:00', '15:00:00', 'HMS')->setToolTip('Hora no formato HH:MM:SS entre 12:00 e 15:00');
+        $frm->addButton('Validar', 'validar', 'btnValidar');
 
-        $this->form->setData( TSession::getValue(__CLASS__.'_filter_data'));
-
-        // add form actions
         // O Adianti permite a Internacionalização - A função _t('string') serve
         //para traduzir termos no sistema. Veja ApplicationTranslator escrevendo
         //primeiro em ingles e depois traduzindo
-        $this->form->addAction(_t('Save'), new TAction(array($this, 'onSave')), 'far:check-circle green');
-        $this->form->addActionLink(_t('Clear'),  new TAction([$this, 'clear']), 'fa:eraser red');
+        $frm->setAction( _t('Save'), 'onSave', null, 'fa:save', 'green' );
+        $frm->setActionLink( _t('Clear'), 'onClear', null, 'fa:eraser', 'red');
+
+        $this->form = $frm->show();
 
         // creates the page structure using a table
         $formDinBreadCrumb = new TFormDinBreadCrumb(__CLASS__);
@@ -44,7 +49,7 @@ class exe_campo_hora extends TPage
     /**
      * Clear filters
      */
-    public function clear()
+    public function onClear()
     {
         $this->clearFilters();
         $this->onReload();
@@ -52,13 +57,24 @@ class exe_campo_hora extends TPage
 
     public function onSave($param)
     {
-        $data = $this->form->getData();
-        $this->form->setData($data);
+        try
+        {
+            $data = $this->form->getData();
+            $this->form->setData($data);
+            $this->form->validate();
+            
+    
+            //Função do FormDin para Debug
+            FormDinHelper::d($param,'$param');
+            FormDinHelper::debug($data,'$data');
+            FormDinHelper::debug($_REQUEST,'$_REQUEST');
 
-        //Função do FormDin para Debug
-        FormDinHelper::d($param,'$param');
-        FormDinHelper::debug($data,'$data');
-        FormDinHelper::debug($_REQUEST,'$_REQUEST');
+            new TMessage('info', 'Tudo OK!');
+        }
+        catch (Exception $e)
+        {
+            new TMessage('error', $e->getMessage());
+        }
     }
 
 }
