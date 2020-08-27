68e0157f2eafddf47df0df164fe7c3b3596b8327
bjverde <bjverde@yahoo.com.br>
2020-08-27 00:42:02 -0300

    :memo: APP #64

--- appexemplo_v1.0/app/control/forms/fields/exe_SelectField_01.php
+++ appexemplo_v1.0/app/control/forms/fields/exe_SelectField_01.php
@@ -19,14 +19,101 @@ class exe_SelectField_01 extends TPage
 
         $frm = new TFormDin($this,'Exemplo do Campo Select Simples');
 
-        $html = '<br><br><b>Regra de Negocio</b>'
-               .'<br>Passe o mouse no balões de ajuda e descubra a regra';
-
-        //$frm->addHtmlField('texto', $html)->setCss('border', '1px solid red');
-        $frm->addHtmlField('texto', $html);
-        $fld = $frm->addTimeField('hor_entrada', 'Hora Entrada:', true, '05:00', '23:00', 'hh:ii');
-        $fld->setToolTip('Hora no formato HH:MM entre 05:00 e 23:00');
-        $frm->addTimeField('hor_inicio2', 'Hora:', false, '12:00:00', '15:00:00', 'HMS')->setToolTip('Hora no formato HH:MM:SS entre 12:00 e 15:00');
+        $frm->addGroupField('gp1', 'Selects Normais');
+        $listFormas = array(1=>'Dinheiro',2=>'Cheque',3=>'Cartão',4=>'BitCoin');
+        $frm->addSelectField('forma_pagamento'     // 1: ID do campo
+                           , 'Forma Pagamento:'
+                           , TRUE
+                           , $listFormas           // 4: array dos valores
+                           , null
+                           , null
+                           , null
+                           , null
+                           , null  //  9: Num itens que irão aparecer
+                           , null  // 10: Largura em Pixels
+                           , ' '   // 11: First Key in Display
+                           , null  // 12: Frist Valeu in Display, use value NULL for required
+                           );
+        
+        $fg2 = $frm->addSelectField('forma_pagamento2'     // 1: ID do campo
+                                   , 'Forma Pagamento (DEFAULT):'
+                                   , TRUE                  // 3: Obrigatorio
+                                   , $listFormas           // 4: array dos valores
+                                   , true                  // 5: Default TRUE = cria nova linha , FALSE = fica depois do campo anterior
+                                   , true                  // 6: Default FALSE = Label mesma linha, TRUE = Label acima
+                                   , null                  // 7: Valor DEFAULT, informe o ID do array
+                                   , null
+                                   , null  //  9: Num itens que irão aparecer
+                                   , 150   // 10: Largura em Pixels
+                                   , null   // 11 First Key in Display
+                                   , null     // 12 Frist Valeu in Display, use value NULL for required
+                                   ,2
+                                   );
+        $fg2->setToolTip('Campo com valor DEFAULT pré-selecionado');
+        
+        $fg2 = $frm->addSelectField('forma_pagamento3'     // 1: ID do campo
+                                , 'Forma Pagamento (DEFAULT):'
+                                , TRUE                  // 3: Obrigatorio
+                                , $listFormas           // 4: array dos valores
+                                , true                  // 5: Default TRUE = cria nova linha , FALSE = fica depois do campo anterior
+                                , true                  // 6: Default FALSE = Label mesma linha, TRUE = Label acima
+                                , null                  // 7: Valor DEFAULT, informe o ID do array
+                                , null
+                                , null  //  9: Num itens que irão aparecer
+                                , 150   // 10: Largura em Pixels
+                                , ' '   // 11 First Key in Display
+                                , 4     // 12 Para o valor DEFAULT informe o ID do $mixOptions e $strFirstOptionText = '' não pode ser null
+                                );
+        $fg2->setToolTip('Campo com valor DEFAULT pré-selecionado, ATRIBUTO 12');
+        
+        
+        $frm->addSelectField('estado', 'Estado (todos):', false, 'tb_uf', true, false, null, false, null, null, '-- selecione o Estado --', null, 'COD_UF', 'NOM_UF', null, 'cod_regiao')->addEvent('onChange', 'select_change(this)')->setToolTip('SELECT - esta campo select possui o código da região adicionado em sua tag option. Para adicionar dados extras a um campo select, basta definir as colunas no parâmetro $arrDataColumns.');
+        $frm->closeGroup();
+        
+        $frm->addGroupField('gp5', 'Selects MultiSelect');
+            $arrayBiomas = array();
+            $arrayBiomas['AM']='Amazônia';
+            $arrayBiomas['CE']='CERRADO';
+            $arrayBiomas['CA']='Caatinga';
+            $arrayBiomas['PA']='PANTANAL';
+            $arrayBiomas['MA']='MATA ATLÂNTICA';
+            $arrayBiomas['PM']='Pampa';
+            $frm->addSelectField('seq_bioma'
+                                , 'Bioma:'
+                                , true
+                                , $arrayBiomas  // 4: array dos valores
+                                , null
+                                , null
+                                , null
+                                , true);
+            
+            $frm->addSelectField('seq_bioma2'
+                                , 'Bioma:'
+                                , false
+                                , $arrayBiomas  // 4: array dos valores
+                                , null
+                                , null
+                ,3  // 7: Valor DEFAULT, informe o ID do array array(0=>3,1=>4)
+                                , true);
+        
+        
+            $frm->addSelectField('estadomultiselect'
+                                , 'Estado (todos):'
+                                , false
+                                , 'tb_uf'      // 4: array dos valores. tb_uf vem do SqLite Configurdo na app de exemplo
+                                , true
+                                , false
+                                , null
+                                , true
+                                , 5          // 9: Num itens que irão aparecer
+                                , null       // 10: Largura em Pixels
+                                , '-- selecione o Estado --'
+                                , null
+                                , 'COD_UF'
+                                , 'NOM_UF'
+                                , null
+                                , 'cod_regiao');
+        $frm->closeGroup();
 
         // O Adianti permite a Internacionalização - A função _t('string') serve
         //para traduzir termos no sistema. Veja ApplicationTranslator escrevendo
