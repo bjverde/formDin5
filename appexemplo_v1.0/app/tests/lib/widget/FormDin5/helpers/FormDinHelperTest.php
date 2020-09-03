f306d793380984b95f78b0da12e2a12f1464408f
bjverde <bjverde@yahoo.com.br>
2020-09-03 00:16:05 -0300

    :memo: new release v5.0.0-alpha16

--- appexemplo_v1.0/app/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
+++ appexemplo_v1.0/app/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
@@ -52,7 +52,7 @@ class FormDinHelperTest extends TestCase
 {
     
     public function testVersion() {
-        $expected = '5.0.0-alpha15';
+        $expected = '5.0.0-alpha16';
         $result =  FormDinHelper::version();
         $this->assertEquals( $expected , $result);
     }
