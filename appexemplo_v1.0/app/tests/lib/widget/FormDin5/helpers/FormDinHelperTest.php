743629ce888340c534c7fa7096d5058ee499f005
bjverde <bjverde@yahoo.com.br>
2020-09-05 15:44:10 -0300

    :hammer: BASE update FormDin Version

--- appexemplo_v1.0/app/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
+++ appexemplo_v1.0/app/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
@@ -52,7 +52,7 @@ class FormDinHelperTest extends TestCase
 {
     
     public function testVersion() {
-        $expected = '5.0.0-alpha16';
+        $expected = '5.0.0-alpha17';
         $result =  FormDinHelper::version();
         $this->assertEquals( $expected , $result);
     }
