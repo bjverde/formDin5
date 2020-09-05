76738e1b701c735ce79352855df8ac55ca671083
bjverde <bjverde@yahoo.com.br>
2020-09-05 15:14:44 -0300

    :memo: #163 melhorando documentação

--- appexemplo_v1.0/app/lib/widget/FormDin5/webform/FormDinGrid/TFormDinGridColumnFormatDate.class.php
+++ appexemplo_v1.0/app/lib/widget/FormDin5/webform/FormDinGrid/TFormDinGridColumnFormatDate.class.php
@@ -52,7 +52,7 @@ class TFormDinGridColumnFormatDate extends TFormDinGridColumn
      * @param  string $label  - 2: Text label that will be shown in the header
      * @param  string $width  - 3: Column Width (pixels)
      * @param  string $align  - 4: Column align (left|right|center|justify)
-     * @param  string $format - 5: Date Format. DEFAULT = d/m/Y (Brazil) , United States = m/d/Y
+     * @param  string $format - 5: Date Format. DEFAULT = d/m/Y (Brazil). Exemplo: United States = m/d/Y. Aceita o formato Adianti dd/mm/yyyy ou DateTime do PHP d/m/Y. 
      * @return BootstrapFormBuilder
      */
     public function __construct(string $name
