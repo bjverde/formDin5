b8fa536de848a2db33f74afc206068d6dc15ab54
bjverde <bjverde@yahoo.com.br>
2020-10-31 17:37:24 -0300

    :hamer: BASE update version 5.0.0-alpha19

--- appexemplo_v1.0/app/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
+++ appexemplo_v1.0/app/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
@@ -52,7 +52,7 @@ class FormDinHelperTest extends TestCase
 {
     
     public function testVersion() {
-        $expected = '5.0.0-alpha18';
+        $expected = '5.0.0-alpha19';
         $result =  FormDinHelper::version();
         $this->assertEquals( $expected , $result);
     }
