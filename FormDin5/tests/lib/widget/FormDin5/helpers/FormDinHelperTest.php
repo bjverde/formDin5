5c0bc509fb8d2cd70daeac0563f5ad59721d5ef6
bjverde <bjverde@yahoo.com.br>
2021-02-01 16:52:54 -0300

    :hammer: BASE new version 5.0.0-alpha20

--- FormDin5/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
+++ FormDin5/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
@@ -52,7 +52,7 @@ class FormDinHelperTest extends TestCase
 {
     
     public function testVersion() {
-        $expected = '5.0.0-alpha19';
+        $expected = '5.0.0-alpha20';
         $result =  FormDinHelper::version();
         $this->assertEquals( $expected , $result);
     }
