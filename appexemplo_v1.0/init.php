6d61530f20072f8b9f5ef83a87880a4b1792f870
bjverde <bjverde@yahoo.com.br>
2021-02-02 22:00:30 -0300

    :hammer: #181 atualizando init.php

--- appexemplo_v1.0/init.php
+++ appexemplo_v1.0/init.php
@@ -20,19 +20,25 @@ ApplicationTranslator::setLanguage( $ini['general']['language'] );
 AdiantiApplicationConfig::load($ini);
 AdiantiApplicationConfig::apply();
 
-// define constants
-define('APPLICATION_NAME', $ini['general']['application']);
-define('OS', strtoupper(substr(PHP_OS, 0, 3)));
-define('PATH', dirname(__FILE__));
-define('LANG', $ini['general']['language']);
-
-
-//---FORMDIN 5 -------------------------
+// ---FORMDIN 5 -------------------------
 define('DS', DIRECTORY_SEPARATOR);
 define('EOL', "\n");
 define('ESP', chr(32).chr(32).chr(32).chr(32) );
 define('TAB', chr(9));
-//---FIM FORMDIN 5 -----------------------
+
+define('SYSTEM_VERSION', $ini['system']['version']);
+define('SYSTEM_NAME', $ini['system']['system_name']);
+// ---FIM FORMDIN 5 -----------------------
+
+// ============= SysGen For Adianti  =================//
+define('ROOT_PATH', '../');
+if(!defined('ROWS_PER_PAGE') ) { 
+    define('ROWS_PER_PAGE', 20); 
+}
+if(!defined('ENCODINGS') ) { 
+    define('ENCODINGS', 'UTF-8'); 
+}
+// ============================================//
 
 // custom session name
 session_name('PHPSESSID_'.$ini['general']['application']);
