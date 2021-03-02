b949d710faaa348410b291ef1a413f5113fc076c
bjverde <bjverde@yahoo.com.br>
2021-03-02 13:40:40 -0300

    :memo: APP #111 primeiro exemplo

---
+++ appexemplo_v1.0/app/control/forms/grid/mockBanco.class.php
@@ -0,0 +1,48 @@
+<?php
+
+class mockBanco
+{
+
+    public static function getExemploAdianti()
+    {
+        $banco = array();
+
+        // add an regular object to the datagrid
+        $item = new StdClass;
+        $item->code   = '1';
+        $item->name   = 'Aretha Franklin';
+        $item->city   = 'Memphis';
+        $item->state  = 'Tennessee (US)';
+        $item->date   = '2010-10-01';
+        $banco[]=$item;
+        
+        // add an regular object to the datagrid
+        $item = new StdClass;
+        $item->code   = '2';
+        $item->name   = 'Eric Clapton';
+        $item->city   = 'Ripley';
+        $item->state  = 'Surrey (UK)';
+        $item->date   = '2010-10-10';
+        $banco[]=$item;
+        
+        // add an regular object to the datagrid
+        $item = new StdClass;
+        $item->code   = '3';
+        $item->name   = 'B.B. King';
+        $item->city   = 'Itta Bena';
+        $item->state  = 'Mississippi (US)';
+        $item->date   = '2010-05-31';
+        $banco[]=$item;
+        
+        // add an regular object to the datagrid
+        $item = new StdClass;
+        $item->code   = '4';
+        $item->name   = 'Janis Joplin';
+        $item->city   = 'Port Arthur';
+        $item->state  = 'Texas (US)';
+        $item->date   = '2020-05-31';
+        $banco[]=$item;
+
+        return $banco;
+    }
+}
