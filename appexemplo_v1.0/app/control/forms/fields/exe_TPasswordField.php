415aec4a20bcc6ff49d351d24d79e757329ecb20
bjverde <bjverde@yahoo.com.br>
2021-05-30 02:29:13 -0300

    :hammer: APP #188 exemplo

--- appexemplo_v1.0/app/control/forms/fields/exe_TPasswordField.php
+++ appexemplo_v1.0/app/control/forms/fields/exe_TPasswordField.php
@@ -16,17 +16,11 @@ class exe_TPasswordField extends TPage
         parent::__construct();
         
         $frm = new TFormDin($this,'Exemplo Campo Senha');
-        //$frm->setMaximize(true);
-        //$frm->setHelpOnLine('Titulo', $strHeight, $strWidth, 'ajuda', null);
-        
-        //$frm->addCpfCnpjField('cpf_cnpj1','CPF/CNPJ:',false)->setExampleText('Mensagem Padrão / Limpar se inconpleto');
 
-        //$frm->addCpfCnpjField('cpf_cnpj2', 'CPF/CNPJ:', false, null, true, null, null, null, null, 'myCb')->setExampleText('Tratamento no callback');
-        /**/
-        //$frm->addCpfCnpjField('cpf_cnpj3', 'CPF/CNPJ:', false, null, true, null, null, 'CPF/CNPJ Inválido', false)->setExampleText('Mensagem Customizada / Limpar se inconpleto');
-        //$frm->addCpfCnpjField('cpf_cnpj4', 'CPF/CNPJ:', false, null, true, null, null, 'CPF/CNPJ Inválido', true)->setExampleText('Mensagem Customizada / Não limpar se inconpleto');
-        $frm->addCpfField('cpf', 'CPF:', false);
-        $frm->addCnpjField('cnpj', 'CNPJ:', false);
+        //$frm->addPasswordField('senha1', 'Senha1:');
+        //$frm->addPasswordField('senha2', 'Senha2:', null, null, null, null, null, null, null, true, true, false);
+        //$frm->addPasswordField('senha3', 'Senha3:', null, null, null, null, null, null, null, true, true, true);
+        //$frm->addPasswordField('senha4', 'Senha4:', null, null, null, null, null, null, null, true, false, true);
 
     
         // O Adianti permite a Internacionalização - A função _t('string') serve
@@ -37,9 +31,7 @@ class exe_TPasswordField extends TPage
 
         $this->form = $frm->show();
 
-
         $this->form->setData( TSession::getValue(__CLASS__.'_filter_data'));
-
 
         // creates the page structure using a table
         $formDinBreadCrumb = new TFormDinBreadCrumb(__CLASS__);
