571cba08425d420d797a727026e30a105be81144
bjverde <bjverde@yahoo.com.br>
2021-10-09 15:25:46 -0300

    :memo: APP mais exemplos

--- appexemplo_v1.0/app/control/forms/fields/exe_campo_cpf_cnpj.php
+++ appexemplo_v1.0/app/control/forms/fields/exe_campo_cpf_cnpj.php
@@ -25,9 +25,25 @@ class exe_campo_cpf_cnpj extends TPage
         /**/
         //$frm->addCpfCnpjField('cpf_cnpj3', 'CPF/CNPJ:', false, null, true, null, null, 'CPF/CNPJ Inválido', false)->setExampleText('Mensagem Customizada / Limpar se inconpleto');
         //$frm->addCpfCnpjField('cpf_cnpj4', 'CPF/CNPJ:', false, null, true, null, null, 'CPF/CNPJ Inválido', true)->setExampleText('Mensagem Customizada / Não limpar se inconpleto');
-        $frm->addCpfField('cpf', 'CPF:', false);
-        $frm->addCnpjField('cnpj', 'CNPJ:', false);
+        $frm->addGroupField('gpx1', 'Basico');
+            $frm->addCpfField('cpf1', 'CPF 1:', false);
+            $frm->addCnpjField('cnpj1', 'CNPJ 1:', false);
+        $frm->closeGroup();
 
+        $frm->addGroupField('gpx2', 'Valor inicial do campo');
+            $frm->addCpfField('cpf2', 'CPF 2:', false,'51263679005');
+            $frm->addCnpjField('cnpj2', 'CNPJ 2:', false,'62781227000179');
+        $frm->closeGroup();
+
+        $frm->addGroupField('gpx3', 'Não valida!');
+            $frm->addCpfField('cpf3', 'CPF 3:', false,'111111111111',null,null,null,null,false);
+            $frm->addCnpjField('cnpj3', 'CNPJ 3:', false,'62781227000100',null,null,null,null,false);
+        $frm->closeGroup();
+
+        $frm->addGroupField('gpx4', 'Envia mascará para o banco');
+            $frm->addCpfField('cpf4', 'CPF 4:', false,'51263679005',null,null,null,null,null,null,null,true);
+            $frm->addCnpjField('cnpj4', 'CNPJ 4:', false,'62781227000179',null,null,null,null,null,null,null,true);
+        $frm->closeGroup();        
     
         // O Adianti permite a Internacionalização - A função _t('string') serve
         //para traduzir termos no sistema. Veja ApplicationTranslator escrevendo
