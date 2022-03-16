50530f86fe39cbbb29ea9446754922d89b5f9936
bjverde <bjverde@yahoo.com.br>
2022-03-16 20:37:18 -0300

    :hammer: BASE #224

--- appexemplo_v1.0/app/tests/phpunit_coverage.bat
+++ appexemplo_v1.0/app/tests/phpunit_coverage.bat
@@ -41,7 +41,10 @@ REM D:\wamp64\bin\php\8.0.13\php.exe D:\wamp64\bin\phpunit\phpunit-9.1.4.phar --
 
 REM ---------------- 9.5.9 -------------------------
 
-ECHO PHP 8.1.0 and PHPUnit 9.5.9 Simples with Coverage
-D:\wamp64\bin\php\php8.1.0\php.exe D:\wamp64\bin\phpunit\phpunit-9.5.9.phar --colors=auto --bootstrap D:\wamp64\www\adianti\formDin5\appexemplo_v1.0\init.php --whitelist D:\wamp64\www\adianti\formDin5\appexemplo_v1.0\app\lib\widget\FormDin5 --coverage-html D:\wamp64\www\adianti\formDin5\phpunit-code-coverage D:\wamp64\www\adianti\formDin5\appexemplo_v1.0\app\tests\
+REM ECHO PHP 8.1.0 and PHPUnit 9.5.9 Simples with Coverage
+REM D:\wamp64\bin\php\php8.1.0\php.exe D:\wamp64\bin\phpunit\phpunit-9.5.9.phar --colors=auto --bootstrap D:\wamp64\www\adianti\formDin5\appexemplo_v1.0\init.php --whitelist D:\wamp64\www\adianti\formDin5\appexemplo_v1.0\app\lib\widget\FormDin5 --coverage-html D:\wamp64\www\adianti\formDin5\phpunit-code-coverage D:\wamp64\www\adianti\formDin5\appexemplo_v1.0\app\tests\
+
+ECHO php 8.1.0 and PHPUnit 9.5.9 with Config XML file
+d:\wamp64\bin\php\php8.1.0\php.exe  d:\wamp64\bin\phpunit\phpunit-9.5.9.phar --configuration D:\wamp64\www\adianti\formDin5\appexemplo_v1.0\app\tests\phpunit-conf-win.xml D:\wamp64\www\adianti\formDin5\appexemplo_v1.0\app\tests\
 
 cd app\tests\
