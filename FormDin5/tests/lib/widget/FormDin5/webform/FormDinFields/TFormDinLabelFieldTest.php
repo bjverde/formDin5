1d3355277a7e122bc52652fa6cb6883b8c52dddf
Reinaldo Araujo Barreto Junior <reinaldo.junior@mpdft.mp.br>
2022-06-27 15:11:57 -0300

    :hammer: #235 update FormDin5

---
+++ FormDin5/tests/lib/widget/FormDin5/webform/FormDinFields/TFormDinLabelFieldTest.php
@@ -0,0 +1,97 @@
+<?php
+/*
+ * ----------------------------------------------------------------------------
+ * Formdin 5 Framework
+ * SourceCode https://github.com/bjverde/formDin5
+ * @author Reinaldo A. Barrêto Junior
+ * 
+ * É uma reconstrução do FormDin 4 Sobre o Adianti 7.X
+ * ----------------------------------------------------------------------------
+ * This file is part of Formdin Framework.
+ *
+ * Formdin Framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not,  see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * Este arquivo é parte do Framework Formdin.
+ *
+ * O Framework Formdin é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * do Software Livre (FSF).
+ *
+ * Este programa é distribuí1do na esperança que possa ser útil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO EM PARTICULAR. Veja a Licen?a Pública Geral GNU/LGPL em portugu?s
+ * para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
+
+$path =  __DIR__.'/../../../../../';
+//require_once $path.'tests/initTest.php';
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Error\Warning;
+
+class TFormDinLabelFieldTest extends TestCase
+{
+
+    private $classTest;
+    
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp(): void {
+        parent::setUp();
+        $this->classTest = new TFormDinLabelField('Label Text',false,'green','20px','b','200');
+    }
+    
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown(): void {
+        $this->classTest = null;
+        parent::tearDown();
+    }     
+    
+    public function test_instanceOff()
+    {
+        $adiantiObj = $this->classTest->getAdiantiObj();
+        $this->assertInstanceOf(TLabel::class, $adiantiObj);
+    }
+
+    public function test_GetClass()
+    {
+        $this->classTest->setClass('aaa');
+        $result =  $this->classTest->getClass();
+        $this->assertEquals('aaa', $result);
+    }
+
+    public function test_GetClass2()
+    {
+        $this->classTest->setClass('aaa');
+        $this->classTest->setClass('bbb');
+        $result =  $this->classTest->getClass();
+        $this->assertEquals('aaa bbb', $result);
+    }
+
+    public function test_GetToottip()
+    {
+        $this->classTest->setTooltip('vaii');
+        $result =  $this->classTest->getTooltip();
+        $this->assertEquals('vaii', $result);
+    }
+}
