17e715da18004ffdc70ed2340d4820c7f2200290
bjverde <bjverde@yahoo.com.br>
2022-08-05 00:24:15 -0300

    versão 5.0.0-alpha29

--- appexemplo_v1.0/app/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
+++ appexemplo_v1.0/app/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
@@ -52,7 +52,7 @@ class FormDinHelperTest extends TestCase
 {
     
     public function testVersion() {
-        $expected = '5.0.0-alpha28';
+        $expected = '5.0.0-alpha29';
         $result =  FormDinHelper::version();
         $this->assertEquals( $expected , $result);
     }
