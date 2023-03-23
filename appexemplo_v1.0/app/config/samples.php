d24aa817339f0b510e3cca70e47a29114844ab8d
bjverde <bjverde@yahoo.com.br>
2023-03-23 01:56:01 -0300

    🐛 #255 banco de exemplo

---
+++ appexemplo_v1.0/app/config/samples.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host'  =>  "",
+    'port'  =>  "",
+    'name'  =>  "app/database/samples.db",
+    'user'  =>  "",
+    'pass'  =>  "",
+    'type'  =>  "sqlite",
+    'prep'  =>  "1"
+];
