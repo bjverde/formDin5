2bd8a974099ba9acf2a9be823c907c160944a929
Bjverde <bjverde@yahoo.com.br>
2023-05-23 21:19:18 -0300

    versão 5.0.0-alpha33

--- FormDin5/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
+++ FormDin5/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
@@ -52,7 +52,7 @@ class FormDinHelperTest extends TestCase
 {
     
     public function testVersion() {
-        $expected = '5.0.0-alpha32';
+        $expected = '5.0.0-alpha33';
         $result =  FormDinHelper::version();
         $this->assertEquals( $expected , $result);
     }
