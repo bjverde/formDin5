cc8f026a800ec6cb2a79b4607cabe6e8e5a6165a
Bjverde <bjverde@yahoo.com.br>
2023-05-23 23:36:47 -0300

    :bug: #259 corrigindo nome do metodo

--- FormDin5/lib/widget/FormDin5/webform/TFormDinDaoDbms.class.php
+++ FormDin5/lib/widget/FormDin5/webform/TFormDinDaoDbms.class.php
@@ -312,9 +312,8 @@ class TFormDinDaoDbms
 	*/
 	public function serializeFields()
 	{
-		if ( $this->getMetadataDir() && $this->getTableName() )
-		{
-		    $filename = $this->getMetadataDir() . $this->getConnDbType() . '-' . $this->getTableName() . '.ser';
+		if ( $this->getMetadataDir() && $this->getTableName() ) {
+		    $filename = $this->getMetadataDir().$this->getType().'-'.$this->getTableName().'.ser';
 		    $data = serialize( $this->getFields() );
 		    file_put_contents( $filename, $data );
 		}
