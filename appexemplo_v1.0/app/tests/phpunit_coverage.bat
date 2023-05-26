bece0fe4b7a6e25a47e67abf98bd686f720ea12f
bjverde <bjverde@yahoo.com.br>
2023-05-26 01:42:54 -0300

    :bug: fix php version

--- appexemplo_v1.0/app/tests/phpunit_coverage.bat
+++ appexemplo_v1.0/app/tests/phpunit_coverage.bat
@@ -41,11 +41,11 @@ REM D:\wamp64\bin\php\8.0.13\php.exe D:\wamp64\bin\phpunit\phpunit-9.1.4.phar --
 
 REM ---------------- 9.5.9 -------------------------
 
-REM ECHO PHP 8.1.0 and PHPUnit 9.5.9 Simples with Coverage
-c:\wamp64\bin\php\php8.1.0\php.exe c:\wamp64\bin\phpunit\phpunit-9.5.9.phar --colors=auto --bootstrap C:\wamp64\www\adinatiApp\formDin5\appexemplo_v1.0\init.php --whitelist C:\wamp64\www\adinatiApp\formDin5\appexemplo_v1.0\app\lib\widget\FormDin5 --coverage-html C:\wamp64\www\adinatiApp\formDin5\phpunit-code-coverage\html C:\wamp64\www\adinatiApp\formDin5\appexemplo_v1.0\app\tests\
+REM ECHO PHP 8.1.13 and PHPUnit 9.5.9 Simples with Coverage
+REM c:\wamp64\bin\php\php8.1.13\php.exe c:\wamp64\bin\phpunit\phpunit-9.5.9.phar --colors=auto --bootstrap C:\wamp64\www\adinatiApp\formDin5\appexemplo_v1.0\init.php --whitelist C:\wamp64\www\adinatiApp\formDin5\appexemplo_v1.0\app\lib\widget\FormDin5 --coverage-html C:\wamp64\www\adinatiApp\formDin5\phpunit-code-coverage\html C:\wamp64\www\adinatiApp\formDin5\appexemplo_v1.0\app\tests\
 
-REM ECHO php 8.1.0 and PHPUnit 9.5.9 with Config XML file
-REM d:\wamp64\bin\php\php8.1.0\php.exe  d:\wamp64\bin\phpunit\phpunit-9.5.9.phar --configuration D:\wamp64\www\adianti\formDin5\appexemplo_v1.0\app\tests\phpunit-conf-win.xml
+ECHO php 8.1.13 and PHPUnit 9.5.9 with Config XML file
+d:\wamp64\bin\php\php8.1.13\php.exe  d:\wamp64\bin\phpunit\phpunit-9.5.9.phar --configuration D:\wamp64\www\adiantiApp\formDin5\appexemplo_v1.0\app\tests\phpunit-conf-win.xml
 
 REM ---------------- 10.0.11 -------------------------
 
