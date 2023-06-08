c0648f4aab3ce823f31b7916015f27275b7a3e30
bjverde <bjverde@yahoo.com.br>
2023-06-08 00:18:26 -0300

    📝 #261 melhorando documentação

--- appexemplo_v1.0/app/lib/widget/FormDin5/webform/TFormDin.class.php
+++ appexemplo_v1.0/app/lib/widget/FormDin5/webform/TFormDin.class.php
@@ -1098,7 +1098,7 @@ class TFormDin
      * @param boolean $enableFileHandling -14: FORMDIN5 Habilita barra de progresso
      * @param boolean $enablePopover      -15: FORMDIN5 Habilita o preview
      * @param integer $enableImageGallery -16: FORMDIN5 Numero da Largura (width) da imagem da galaria, DEFAULT = 120. Para customizar use o metodo enableImageGallery
-     * @param boolean $enableMultiFile    -17: Muda de Upload simples para Upload Multiplos
+     * @param boolean $enableMultiFile    -17: FORMDIN5 Muda de Upload simples para Upload Multiplos
      * @return TFile|TFileAsync
      */
     public function addFileField(string $id
