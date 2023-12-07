125a857bf3e2134d3f6e4c621262b27d07ec7835
Bjverde <bjverde@yahoo.com.br>
2023-12-07 12:46:30 -0300

    🔨 APP #281 limpando exemplo

--- appexemplo_v1.0/app/control/forms/fields/exe_upload_img.php
+++ appexemplo_v1.0/app/control/forms/fields/exe_upload_img.php
@@ -17,69 +17,11 @@ class exe_upload_img extends TPage
 
         $frm = new TFormDin($this,'Exemplo Upload Imagem');
         $frm->addHiddenField('id'); //POG para evitar problema de noticie
-        $frm->addVideoStreamPhoto('selfieponto','Nova WebCam',true);
+        $frm->addVideoStreamPhoto('webcam1','Nova WebCam',true);
 
-        /*
-        $idField = 'selfieponto';
-
-        $adiantiObjHiden = new THidden($idField);
-        $adiantiObjHiden->setId($idField);
-
-        $adiantiObjWebCam = new TElement('video');
-        $adiantiObjWebCam->class = 'fd5Video';
-        $adiantiObjWebCam->setProperty('id',$idField.'_video');
-        $adiantiObjWebCam->setProperty('name',$idField.'_video');        
-        $adiantiObjWebCam->add('autoplay');
-        $adiantiObjWebCam->add('Your browser does not support HTML video.');
-
-        $adiantiObjWebCamCanvas = new TElement('canvas');
-        $adiantiObjWebCamCanvas->class = 'fd5WebCamCanvas';
-        $adiantiObjWebCamCanvas->setProperty('id',$idField.'_videoCanvas');
-        $adiantiObjWebCamCanvas->setProperty('name',$idField.'_videoCanvas');          
-
-        $scriptJswebCam = new TElement('script');
-        $scriptJswebCam->setProperty('src', 'app/lib/include/FormDin5WebCams.js?appver='.FormDinHelper::version());
-
-
-        $btnStart = new TButton('btnStart');
-        $btnStart->class = 'btn btn-success btn-sm';
-        $btnStart->setLabel('Ligar Câmera');
-        $btnStart->setImage('fa:power-off');
-        $btnStart->addFunction("fd5VideoStart('".$idField."')");
-
-        $btnChangeCamera = new TButton('btnChangeCamera');
-        $btnChangeCamera->class = 'btn btn-light btn-sm';
-        $btnChangeCamera->setLabel('Alterar Camera');
-        $btnChangeCamera->setImage('fa:sync-alt');
-        $btnChangeCamera->addFunction("fd5VideoCampiturar('".$idField."')");
-
-        $btnScreenshot = new TButton('btnScreenshot');
-        $btnScreenshot->class = 'btn btn-primary btn-sm';
-        $btnScreenshot->setLabel('Capiturar Foto');
-        $btnScreenshot->setImage('fa:camera');
-        $btnScreenshot->addFunction("fd5VideoCampiturar('".$idField."')");
-
-        $divButton = new TElement('div');
-        $divButton->class = 'fd5DivVideoButton';
-        $divButton->setProperty('id',$idField.'_videoDivButton');
-        $divButton->add($btnStart);
-        //$divButton->add($btnChangeCamera);
-        $divButton->add($btnScreenshot);
-
-
-        $divWebCam = new TElement('div');
-        $divWebCam->class = 'fd5DivVideo';
-        $divWebCam->setProperty('id',$idField.'_videodiv');
-        $divWebCam->add($adiantiObjHiden);
-        $divWebCam->add($adiantiObjWebCam);
-        $divWebCam->add($adiantiObjWebCamCanvas);
-        $divWebCam->add($scriptJswebCam);
-        $divWebCam->add($divButton);
-        */
-
-        //$fd5VideoStream = new TFormDinVideoStreamPhoto('selfieponto','Nova WebCam',true);
+        //$fd5VideoStream = new TFormDinVideoStreamPhoto('webcam2','WebCam modo Adianti',true);
         //$divWebCam = $fd5VideoStream->getAdiantiObj();      
-        //$row5 = $frm->addFields([new TLabel("Nova WebCam:", '#ff0000', '14px', null, '100%')],[$divWebCam]);
+        //$row5 = $frm->addFields([new TLabel("WebCam modo Adianti:", '#ff0000', '14px', null, '100%')],[$divWebCam]);
 
         $this->form = $frm->show();
         $this->form->setData( TSession::getValue(__CLASS__.'_filter_data'));
