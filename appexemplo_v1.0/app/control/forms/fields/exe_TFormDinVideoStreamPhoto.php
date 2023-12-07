f4805948e9c4c3238ec77341acacf9da4e810e22
Bjverde <bjverde@yahoo.com.br>
2023-12-07 13:06:32 -0300

    🔨 APP #281 melhorando exemplo

--- appexemplo_v1.0/app/control/forms/fields/exe_TFormDinVideoStreamPhoto.php
+++ appexemplo_v1.0/app/control/forms/fields/exe_TFormDinVideoStreamPhoto.php
@@ -17,11 +17,29 @@ class exe_TFormDinVideoStreamPhoto extends TPage
 
         $frm = new TFormDin($this,'Exemplo TFormDinVideoStreamPhoto');
         $frm->addHiddenField('id'); //POG para evitar problema de noticie
+
+        $msg = null;
+        $msg = $msg.'<br>No Adianti tem os exemplos FormImageUploader com TImageCapture que pegar da Camera';
+        $msg = $msg.'<br>Porém no celular o TImageCapture permite fazer upload de imagens, porém o TFormDinVideoStreamPhoto só usar Camera';
+        $msg = $msg.'<br><a href="https://framework.adianti.me//tutor/index.php?class=FormImageUploader">Adianti Tutor - FormImageUploader</a>';
+        $frm->addHtmlField('dica1',$msg, null, 'Dica:', null, 200);
+
+
         $frm->addVideoStreamPhoto('webcam1','Nova WebCam',true);
 
         //$fd5VideoStream = new TFormDinVideoStreamPhoto('webcam2','WebCam modo Adianti',true);
         //$divWebCam = $fd5VideoStream->getAdiantiObj();      
         //$row5 = $frm->addFields([new TLabel("WebCam modo Adianti:", '#ff0000', '14px', null, '100%')],[$divWebCam]);
+
+        $msg = null;
+        $msg = $msg.'<br>Exemplo com upload de imagens';
+        $msg = $msg.'<ul>';
+        $msg = $msg.'  <li><a href="index.php?class=exe_fwShowBlobDisco">Exemplo Campo Select Simples</a></li>';
+        $msg = $msg.'  <li><a href="index.php?class=exe_TFileMulti">Exemplo campo upload arquivo Multiplo</a></li>';
+        $msg = $msg.'  <li><a href="index.php?class=exe_TFile">Exemplo campo upload arquivo</a></li>';
+        $msg = $msg.'</ul>';
+        $frm->addGroupField('gpx1', 'Veja outros exemplos relacionados');
+        $frm->addHtmlField('aviso_relacionado',$msg, null, 'Lista de exemplos', null, 200);        
 
         $this->form = $frm->show();
         $this->form->setData( TSession::getValue(__CLASS__.'_filter_data'));
