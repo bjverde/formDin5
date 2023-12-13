4656bf62f4494040f4877d49a390c6e91a3698b6
Bjverde <bjverde@yahoo.com.br>
2023-12-13 02:59:25 -0300

    :bug: #281 alternando entre elementos

--- appexemplo_v1.0/app/lib/include/FormDin5WebCams.js
+++ appexemplo_v1.0/app/lib/include/FormDin5WebCams.js
@@ -72,6 +72,15 @@ function fd5VideoStop(id){
   }
 }
 
+function fd5VideoAlternarDisplay(idElemento, modoExibicao) {
+  var elemento = document.querySelector(idElemento);
+  // Verifica se o modo de exibição atual é igual ao desejado
+  if (window.getComputedStyle(elemento).display === modoExibicao) {
+      return;
+  }
+  elemento.style.display = modoExibicao; // Alterna para o modo de exibição desejado
+}
+
 
 function fd5VideoStart(id){
    if ( !"mediaDevices" in navigator ||
@@ -80,10 +89,9 @@ function fd5VideoStart(id){
     return;
   }
   fd5VideoStop(id);
-  let canvas= document.querySelector('#'+id+'_videoCanvas');
-  canvas.style.display = "none";
+  fd5VideoAlternarDisplay('#'+id+'_videoCanvas','none');
   let video = document.querySelector('#'+id+'_video');
-  video.style.display = "block";
+  fd5VideoAlternarDisplay('#'+id+'_video','block');
 
 	navigator.mediaDevices.getUserMedia({video:true})
 	.then(stream => {
@@ -203,10 +211,12 @@ function fd5VideoSaveTmpAdianti(id,canvas){
  */
 function fd5VideoCampiturar(id){
   try {
-    var video  = document.querySelector('#'+id+'_video');
-    video.style.display = "nome";
-    var canvas = document.querySelector('#'+id+'_videoCanvas');
-    canvas.style.display = "block";
+    let idVideo= '#'+id+'_video';
+    var video  = document.querySelector(idVideo);
+    fd5VideoAlternarDisplay(idVideo,'none');
+    let idCanvas= '#'+id+'_videoCanvas';
+    var canvas  = document.querySelector(idCanvas);
+    fd5VideoAlternarDisplay(idCanvas,'block');
     var context= canvas.getContext('2d');
 
     //video.style.display = 'none';
