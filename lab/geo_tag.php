97ea005a253d99590db4e0bf7787c0e74c526ee0
Bjverde <bjverde@yahoo.com.br>
2023-12-28 20:25:59 -0300

    :hammer: #55 entendimento basico

--- lab/geo_tag.php
+++ lab/geo_tag.php
@@ -24,43 +24,18 @@ function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition,showError);
     } else{
-        x.innerHTML="Geolocalização não é suportada nesse browser.";
+        console.log('Geolocalização não é suportada nesse browser.');
     }
 }
- 
 function showPosition(position) {
-    lat=position.coords.latitude;
-    lon=position.coords.longitude;
-    latlon=new google.maps.LatLng(lat, lon)
-    mapholder=document.getElementById('mapholder')
-    mapholder.style.height='250px';
-    mapholder.style.width='500px';
- 
-    var myOptions={
-        center:latlon,zoom:14,
-        mapTypeId:google.maps.MapTypeId.ROADMAP,
-        mapTypeControl:false,
-        navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
-    };
-    var map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
-    var marker=new google.maps.Marker({position:latlon,map:map,title:"Você está Aqui!"});
+    console.log(position);
+    let lat=position.coords.latitude;
+    let lon=position.coords.longitude;
+    console.log('lat',lat);
+    console.log('lon',lon);
 }
- 
 function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-        x.innerHTML="Usuário rejeitou a solicitação de Geolocalização."
-        break;
-        case error.POSITION_UNAVAILABLE:
-        x.innerHTML="Localização indisponível."
-        break;
-        case error.TIMEOUT:
-        x.innerHTML="O tempo da requisição expirou."
-        break;
-        case error.UNKNOWN_ERROR:
-        x.innerHTML="Algum erro desconhecido aconteceu."
-        break;
-    }
+    console.log(error);
 }
 </script>
 </body>
