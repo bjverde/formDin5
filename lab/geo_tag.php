e9028147e7f76815b9e5bc100f28346ddb28b861
Bjverde <bjverde@yahoo.com.br>
2023-12-29 20:08:12 -0300

    🔨 #55 gerando json de tudo

--- lab/geo_tag.php
+++ lab/geo_tag.php
@@ -26,8 +26,18 @@ function showPosition(position) {
     console.log(position);
     let latitude=position.coords.latitude;
     let longitude=position.coords.longitude;
-    console.log('Latitude: ',latitude);
-    console.log('Longitude:',longitude);
+    var dadosLocalizacao = {
+         latitude: position.coords.latitude
+        ,longitude: position.coords.longitude
+        ,altitude: position.coords.altitude
+        ,accuracy: position.coords.accuracy
+        ,altitudeAccuracy: position.coords.altitudeAccuracy
+        ,heading: position.coords.heading
+        ,speed: position.coords.speed
+        ,timestamp: position.timestamp
+    };
+    var jsonLocalizacao = JSON.stringify(dadosLocalizacao);
+    console.log('Localização em formato JSON:', jsonLocalizacao);
 }
 function showError(error) {
     console.log('Erro ao obter localização:', erro);
