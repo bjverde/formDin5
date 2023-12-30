61c2ec04ace737434d44c08d58c359fc748ea61a
Bjverde <bjverde@yahoo.com.br>
2023-12-30 00:16:39 -0300

    🔨 BASE #55 JS criando arquivo

---
+++ appexemplo_v1.0/app/lib/widget/FormDin5/javascript/FormDin5GeoLocation.js
@@ -0,0 +1,30 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition,showError);
+    } else{
+        console.log('Geolocalização não é suportada nesse browser.');
+    }
+}
+function showPosition(position) {
+    console.log(position);
+    let latitude=position.coords.latitude;
+    let longitude=position.coords.longitude;
+    var dadosLocalizacao = {
+         latitude: position.coords.latitude
+        ,longitude: position.coords.longitude
+        ,altitude: position.coords.altitude
+        ,accuracy: position.coords.accuracy
+        ,altitudeAccuracy: position.coords.altitudeAccuracy
+        ,heading: position.coords.heading
+        ,speed: position.coords.speed
+        ,timestamp: position.timestamp
+    };
+    var jsonLocalizacao = JSON.stringify(dadosLocalizacao);
+    console.log('Localização em formato JSON:', jsonLocalizacao);
+}
+function showError(error) {
+    console.log('Erro ao obter localização:', erro);
+}
+
+// Atribuir a função diretamente a window.onload
+window.onload = getLocation;
