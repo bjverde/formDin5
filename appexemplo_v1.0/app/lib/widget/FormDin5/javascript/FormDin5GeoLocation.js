2d2b2fca64c1dea1ba4668f1dcebeaba87046bfd
Bjverde <bjverde@yahoo.com.br>
2023-12-30 14:56:49 -0300

    🔨 BASE #55 JS setando mensagem de erro

--- appexemplo_v1.0/app/lib/widget/FormDin5/javascript/FormDin5GeoLocation.js
+++ appexemplo_v1.0/app/lib/widget/FormDin5/javascript/FormDin5GeoLocation.js
@@ -7,7 +7,14 @@ function fd5GetLocation(idField) {
             fd5ShowPosition(position,idField);
         },fd5ShowPositionError);
     } else{
-        console.log('Geolocalização não é suportada nesse browser.');
+        let dadosLocalizacao = {
+            code: 999
+           ,msg :'Geolocalização não é suportada nesse browser.'
+        };
+       let jsonLocalizacao = JSON.stringify(dadosLocalizacao);
+       let fieldJson = document.querySelector('#'+idField+'_json');
+       fieldJson.value = jsonLocalizacao;
+       console.log(code,msg);
     }
 }
 function fd5ShowPosition(position,idField) {
@@ -39,5 +46,28 @@ function fd5ShowPosition(position,idField) {
     fieldAlt.value = altitude;
 }
 function fd5ShowPositionError(error) {
-    console.log('Erro ao obter localização:', error);
+    var code= error.code;
+    var msg = null;
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            msg = "Permissão negada para acessar a localização.";
+        break;
+        case error.POSITION_UNAVAILABLE:
+            msg = "Localização indisponível.";
+        break;
+        case error.TIMEOUT:
+            msg = "Tempo esgotado para obter a localização.";
+        break;
+        default:
+            msg = "Ocorreu um erro desconhecido ao obter a localização.";
+        break;
+    }
+    let dadosLocalizacao = {
+        code:code
+       ,msg:msg
+    };
+   let jsonLocalizacao = JSON.stringify(dadosLocalizacao);
+   let fieldJson = document.querySelector('#'+idField+'_json');
+   fieldJson.value = jsonLocalizacao;
+   console.log(code,msg);
 }
