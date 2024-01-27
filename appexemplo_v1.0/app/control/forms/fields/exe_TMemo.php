21e560dcd5e90093ca3e0c20ab35bf1d040a6272
Bjverde <bjverde@yahoo.com.br>
2024-01-27 20:52:44 -0300

    :hammer: #292 APP voltando o exemplo anterior

--- appexemplo_v1.0/app/control/forms/fields/exe_TMemo.php
+++ appexemplo_v1.0/app/control/forms/fields/exe_TMemo.php
@@ -13,19 +13,19 @@ class exe_TMemo extends TPage
 
     public function __construct()
     {
-        parent::__construct();        
+        parent::__construct();
 
-        $frm = new TFormDin($this,'Exemplo Campo Rich Editor');
+        $frm = new TFormDin($this,'Exemplo Campo Memo');
         $frm->addHiddenField('id'); //POG para evitar problema de noticie
-        $frm->addRichTextEditor('memo1NaoObri', 'Não Obrigatório:', 365, false,'100%');
-        $frm->addRichTextEditor('memo1Obri', 'Obrigatório:', 365, true,'100%');
-        $frm->addRichTextEditor('memo2', 'Memo2:', 50, false,'100%', '10%')->setPlaceHolder('Texto de exemplo 100% largura com 10% de altura');
-        $memo3 = $frm->addRichTextEditor('memo3', 'Memo3:', 50, false,'100%', '10%',false);
+        $frm->addMemoField('memo1NaoObri', 'Não Obrigatório:', 365, false,'100%');
+        $frm->addMemoField('memo1Obri', 'Obrigatório:', 365, true,'100%');
+        $frm->addMemoField('memo2', 'Memo2:', 50, false,'100%', '10%')->setPlaceHolder('Texto de exemplo 100% largura com 10% de altura');
+        $memo3 = $frm->addMemoField('memo3', 'Memo3:', 50, false,'100%', '10%',false);
         $memo3->setReadOnly(true);
         $memo3->setPlaceHolder('Campo somente leitura com Place Holder. Texto de exemplo 100% largura com 10% de altura');
-        $frm->addRichTextEditor('memo4', 'Memo4:', 400, false, 120, 30,true);
-        $frm->addRichTextEditor('memo5', 'Memo5:', 400, false,'80%', '15%',false,null,null,'Texto já preenchido');
-        //$frm->addRichTextEditor('memo5', 'Memo5:', 400, false,'30%', '10%',null,true,null,'Texto já preenchido');
+        $frm->addMemoField('memo4', 'Memo4:', 400, false, 120, 30,true);
+        $frm->addMemoField('memo5', 'Memo5:', 400, false,'80%', '15%',false,null,null,'Texto já preenchido');
+        //$frm->addMemoField('memo5', 'Memo5:', 400, false,'30%', '10%',null,true,null,'Texto já preenchido');
         
         // O Adianti permite a Internacionalização - A função _t('string') serve
         //para traduzir termos no sistema. Veja ApplicationTranslator escrevendo
