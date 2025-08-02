4157df4559a88f62da51473fd5ddda5b590d831a
Bjverde <bjverde@yahoo.com.br>
2025-08-02 03:22:46 -0300

    :hammer: APP #322 melhorando exemplo de coordenada com feedback

--- appexemplo_v1.0/app/control/forms/fields/exe_TFormDinCordLatLon.php
+++ appexemplo_v1.0/app/control/forms/fields/exe_TFormDinCordLatLon.php
@@ -22,14 +22,15 @@ class exe_TFormDinCordLatLon extends TPage
         $frm->addHiddenField('id'); //POG para evitar problema de noticie
 
 
-        $frm->addCordLatLon('cood','Coordenadas',false,false,false);
+        $frm->addCordLatLon('cood','Coordenadas',false,true,false);
 
         $msg = 'Veja classe TFormDinGeo com metodos';
         $msg = $msg.'<br>';
         $msg = $msg.'<br>isPointInQuadrilateral - Verificar se um ponto (latitude e longitude) está dentro de um quadrilatero';
         $msg = $msg.'<br>isPointWithinRadius - Verificar se um ponto (latitude e longitude) está em raio em metro de um ponto de referencia';
         
-        $frm->addHtmlField('html1', $msg, null, 'Dica:', null, 200)->setClass('notice'); 
+        $frm->addHtmlField('html1', $msg, null, 'Dica:', null, 200,true)->setClass('notice');
+        $frm->addCordLatLon('cood2','Coordenadas 2',false,true,false,false);
 
 
         $this->form = $frm->show();
