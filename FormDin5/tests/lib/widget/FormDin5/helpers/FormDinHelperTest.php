2da2aa8ef7442dc8449eb8767e51bc70a6a0fe9f
Bjverde <bjverde@yahoo.com.br>
2026-02-18 23:23:27 -0300

    :rocket: versão 5.8.0

--- FormDin5/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
+++ FormDin5/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
@@ -50,7 +50,7 @@ use PHPUnit\Framework\TestCase;
 
 class FormDinHelperTest extends TestCase
 {
-    public $formDinVersion = '5.7.0';
+    public $formDinVersion = '5.8.0';
     public $adiantiVersion = '7.6.0.4';
 
     public function testVersion() {
