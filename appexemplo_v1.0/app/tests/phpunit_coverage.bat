fede2b8e780aad7b7416b66801c64e7a5c51d290
Bjverde <bjverde@yahoo.com.br>
2026-05-26 02:12:54 -0300

    :bug: correção caminho

--- appexemplo_v1.0/app/tests/phpunit_coverage.bat
+++ appexemplo_v1.0/app/tests/phpunit_coverage.bat
@@ -4,8 +4,8 @@ ECHO Teste PhpUnit
 cd ../../
 
 REM ---------------- 12.3.6 -------------------------
-ECHO PHP 8.3.6 and PHPUnit 12.3.6 Simples with Coverage
-d:\wamp64\bin\php\php8.3.6\php.exe d:\wamp64\bin\phpunit\phpunit-12.3.6.phar --coverage-html app\tests\phpunit-reports\code-coverage --testdox-html app\tests\phpunit-reports\code-report.html
+ECHO PHP 8.3.28 and PHPUnit 12.3.6 Simples with Coverage
+d:\wamp64\bin\php\php8.3.28\php.exe d:\wamp64\bin\phpunit\phpunit-12.3.6.phar --coverage-html app\tests\phpunit-reports\code-coverage --testdox-html app\tests\phpunit-reports\code-report.html
 
 
 cd app\tests\
