f3a8d789a0bc0eedc4782cda4dc46fd87f7a5013
Bjverde <bjverde@yahoo.com.br>
2026-06-04 16:41:01 -0300

    :hammer: APP #337 novos arquivos

---
+++ appexemplo_v1.0/app/config/application.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    'general' =>  [
+        'timezone' => 'America/Sao_Paulo',
+        'language' => 'pt',
+        'application' => 'sample',
+        'title' => 'Adianti Framework 8.1',
+        'theme' => 'adminbs5_v3',
+        'debug' => '1',
+        'strict_request' => '0'
+    ],
+    'template' => [
+        'navbar' => [
+            'has_menu_mode_switch' => '1',
+            'has_main_mode_switch' => '1'
+        ],
+        'dialogs' => [
+            'use_swal' => '1'
+        ],
+        'theme' => [
+            'menu_mode' => 'dark',
+            'main_mode' => 'light'
+        ],
+    ],
+    'system' =>  [
+        'system_version' => '8.1.0.1',
+        'system_name_sub'=> 'Fork do Adianti FrameWork',
+        'adianti_min_version'=> '8.0.0',
+        'formdin_min_version'=> '5.4.0',
+    ],
+];
