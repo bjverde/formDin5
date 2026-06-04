5f2b8799cd40f58ae9fcebaeb2e87c95455ca98e
Bjverde <bjverde@yahoo.com.br>
2026-06-04 12:16:52 -0300

    🔨 APP #336 script de teste

--- appexemplo_v1.0/app/tests/phpunit_coverage.bat
+++ appexemplo_v1.0/app/tests/phpunit_coverage.bat
@@ -4,8 +4,12 @@ ECHO Teste PhpUnit
 cd ../../
 
 REM ---------------- 12.3.6 -------------------------
-ECHO PHP 8.3.28 and PHPUnit 12.3.6 Simples with Coverage
-d:\wamp64\bin\php\php8.3.28\php.exe d:\wamp64\bin\phpunit\phpunit-12.3.6.phar --coverage-html app\tests\phpunit-reports\code-coverage --testdox-html app\tests\phpunit-reports\code-report.html
+REM ECHO PHP 8.3.28 and PHPUnit 12.3.6 Simples with Coverage
+REM d:\wamp64\bin\php\php8.3.28\php.exe d:\wamp64\bin\phpunit\phpunit-12.3.6.phar --coverage-html app\tests\phpunit-reports\code-coverage --testdox-html app\tests\phpunit-reports\code-report.html
+
+REM ---------------- 13.1.14 -------------------------
+ECHO PHP 8.3.28 and PHPUnit 12.5.29 Simples with Coverage
+d:\wamp64\bin\php\php8.3.28\php.exe d:\wamp64\bin\phpunit\phpunit-12.5.29.phar --coverage-html app\tests\phpunit-reports\code-coverage --testdox-html app\tests\phpunit-reports\code-report.html
 
 
 cd app\tests\
