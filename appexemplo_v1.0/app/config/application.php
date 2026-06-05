901878254968fecb616c49d35b3a194db372af9d
Bjverde <bjverde@yahoo.com.br>
2026-06-04 21:10:43 -0300

    :hammer: APP #337 alterando config

--- appexemplo_v1.0/app/config/application.php
+++ appexemplo_v1.0/app/config/application.php
@@ -23,9 +23,9 @@ return [
         ],
     ],
     'system' =>  [
-        'system_version' => '8.1.0.1',
+        'system_version' => '5.11.0',
         'system_name_sub'=> 'Aplicação de Exemplo 1 do FormDin5 com Adianti',
-        'adianti_min_version'=> '8.1.0',
+        'adianti_min_version'=> '8.4.0',
         'formdin_min_version'=> '5.10.0',
     ],
 ];
