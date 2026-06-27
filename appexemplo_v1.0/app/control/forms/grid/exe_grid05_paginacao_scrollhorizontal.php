abf10f63bdbc1f7b99381e4df919c6088a66d706
Bjverde <bjverde@yahoo.com.br>
2026-06-27 16:38:50 -0300

    🚚 APP #227 pradronizando nome classe e menu de gridE para grid - exe_grid05_paginacao

--- appexemplo_v1.0/app/control/forms/grid/exe_grid05_paginacao_scrollhorizontal.php
+++ appexemplo_v1.0/app/control/forms/grid/exe_grid05_paginacao_scrollhorizontal.php
@@ -2,7 +2,7 @@
 
 use Adianti\Registry\TSession;
 
-class exe_gride05_paginacao_scrollhorizontal extends TPage
+class exe_grid05_paginacao_scrollhorizontal extends TPage
 {
     protected $form; // registration form
     protected $datagrid; // listing
