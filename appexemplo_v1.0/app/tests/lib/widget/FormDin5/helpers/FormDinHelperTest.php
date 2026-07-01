9e772aba17cea2cfea36cb9401a707fd5cc59973
Bjverde <bjverde@yahoo.com.br>
2026-06-30 22:20:39 -0300

    🚀 versão 5.13.0

--- appexemplo_v1.0/app/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
+++ appexemplo_v1.0/app/tests/lib/widget/FormDin5/helpers/FormDinHelperTest.php
@@ -50,7 +50,7 @@ use PHPUnit\Framework\TestCase;
 
 class FormDinHelperTest extends TestCase
 {
-    public $formDinVersion = '5.12.00';
+    public $formDinVersion = '5.13.00';
     public $adiantiVersion = '8.4.0.1';
 
     public function testVersion() {
