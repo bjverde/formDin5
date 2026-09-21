a62abb88a9fb0671225546cf7a8062ea4bbd03aa
Reinaldo Araujo Barreto Junior <reinaldo.junior@mpdft.mp.br>
2026-09-21 14:47:30 -0300

    🚀 versão 5.14.4

--- appexemplo_v1.0/app/config/application.php
+++ appexemplo_v1.0/app/config/application.php
@@ -23,9 +23,9 @@ return [
         ],
     ],
     'system' =>  [
-        'system_version' => '5.14.3',
+        'system_version' => '5.14.4',
         'system_name_sub' => 'Aplicação de Exemplo 1 do FormDin5 com Adianti',
         'adianti_min_version' => '8.4.0',
-        'formdin_min_version' => '5.14.3',
+        'formdin_min_version' => '5.14.4',
     ],
 ];
